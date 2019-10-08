c7eab9b7ebcf72b9af5582ce8c93b4532c94f5a7
Jacob Wunder <jacob@jacobwunder.com>
2019-10-08 10:46:18 -0400

    Initialize Satics Engine Library

---
+++ StaticsEngine/src/main/java/com/jacobwunder/staticsengine/StaticsEngine.java
@@ -0,0 +1,23 @@
+package com.jacobwunder.staticsengine;
+
+public class StaticsEngine {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+
+    /*
+    * This should take the force, location (as a fraction of the length)
+    * and point of interest (as a fraction of the length)
+    * and calculate the deflection of the beam at the given point of interest
+    *
+    * You should assume as many variable as you can, and represent
+    * them as constants in this class
+    *
+    * */
+    double simulate(double force, double location, double poi) {
+
+        return 0;
+    }
+
+}
