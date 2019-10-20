df7d1e74797a504238c295f758dd4a97ef059d5b
Jacob Wunder <jacob@jacobwunder.com>
2019-10-20 16:32:09 -0400

    Update Force to add location field

--- libstatics/src/main/java/com/jacobwunder/libstatics/Force.java
+++ libstatics/src/main/java/com/jacobwunder/libstatics/Force.java
@@ -3,6 +3,13 @@ package com.jacobwunder.libstatics;
 public class Force {
     private double magnitude;
     private double angle;
+    private Point location;
+
+    public Force(double magnitude, double angle, Point location) {
+        this.magnitude = magnitude;
+        this.angle = angle;
+        this.location = location;
+    }
 
     public double getMagnitude() {
         return magnitude;
@@ -20,7 +27,7 @@ public class Force {
         this.angle = angle;
     }
 
+    public Point getLocation() { return location; }
 
-
-
+    public void setLocation(Point location) { this.location = location; }
 }
